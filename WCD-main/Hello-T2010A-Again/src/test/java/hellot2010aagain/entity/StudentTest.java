51ea804fd6ee94777635fa4404fa71d5843efb93
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2022-05-25 00:04:54 +0700

    Update StudentTest.java

--- WCD-main/Hello-T2010A-Again/src/test/java/hellot2010aagain/entity/StudentTest.java
+++ WCD-main/Hello-T2010A-Again/src/test/java/hellot2010aagain/entity/StudentTest.java
@@ -11,9 +11,9 @@ class StudentTest {
     public void testCreateStudent() {
         Student student = new Student();
         student.setRollNumber("A001");
-        student.setFullName("Xuan Hung");
-        student.setEmail("hung@gmail.com");
-        student.setPhone("091234567");
+        student.setFullName("Viet Hoang");
+        student.setEmail("hoang@gmail.com");
+        student.setPhone("0854973819");
         student.setDob(LocalDateTime.of(2004, 10, 22, 10, 10));
         student.setCreatedAt(LocalDateTime.now());
         student.setUpdatedAt(LocalDateTime.now());
